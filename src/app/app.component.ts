3f9782b083eef90e638d4442f821c745efcc6ba9
kevin-mehta <kevin.h.mehta@gmail.com>
2020-09-29 11:55:52 +0530

    Implemented ESC button functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, AfterViewInit } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements AfterViewInit {
+  name = "Angular " + VERSION.major;
+
+  fromEvent(target: HTMLInputElement, eventName: string) {
+    return new Observable(observer => {
+      const handler = (e: unknown) => observer.next(e);
+      target.addEventListener(eventName, handler);
+      return () => {
+        target.removeEventListener(eventName, handler);
+      };
+    });
+  }
+
+  ngAfterViewInit() {
+    const ESC_KEY = 27;
+    const nameInput = document.getElementById("yourname") as HTMLInputElement;
+    this.fromEvent(nameInput, "keydown").subscribe((e: KeyboardEvent) => {
+      if (e.keyCode === ESC_KEY) {
+        nameInput.value = "";
+      }
+    });
+  }
 }
